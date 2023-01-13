fd641b74d0312a8e0f5c574b0ece8b6611ab5c6e
Liisa Lahti <liisa.lahti@futurice.com>
2023-01-13 13:36:29 +0200

    Add constants

---
+++ utils/constants.ts
@@ -0,0 +1,19 @@
+export const mobile = {
+  border: 2,
+  cellInner: 22,
+  cellWithBorder: 24,
+  cellWithBorders: 26,
+  columns: 20,
+  headerRows: 9,
+  rows: 40,
+}
+
+export const desktop = {
+  border: 4,
+  cellInner: 44,
+  cellWithBorder: 48,
+  cellWithBorders: 52,
+  columns: 20,
+  headerRows: 8,
+  rows: 20,
+}
