34f2f4651c1cf063b68c5fbc1b1f9153e301a464
Liisa Lahti <liisa.lahti@futurice.com>
2023-01-13 13:36:19 +0200

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/beach'
+  basePath: '/beach',
+  compiler: {
+    styledComponents: true
+  }
 }
 
 module.exports = nextConfig
